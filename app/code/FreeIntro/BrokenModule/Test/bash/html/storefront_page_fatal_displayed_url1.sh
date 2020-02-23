97dbaf95a7cf52681830b90822026318a8d813b3
Santiago Casanova <tyagocasanova@hotmail.com>
2020-02-23 21:19:29 +0100

    Eliminadas las carpetas de code practicas

--- app/code/FreeIntro/BrokenModule/Test/bash/html/storefront_page_fatal_displayed_url1.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-moduleName="BrokenModule"
-moduleGroup="FreeIntro"
-testDesc="Fatal Error Page"
-pageId="fixmodule/storefront/page"
-matchRule="Fatal error"
-
-test_html_page_match "$moduleName" "$moduleGroup" "$testDesc" "$pageId" "$matchRule"
--- app/code/FreeIntro/NewAdminpage/Test/bash/html/admin/page_displayed.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-moduleName="NewAdminpage"
-moduleGroup="FreeIntro"
-testDesc="Custom Admin Page is Displayed"
-matchRule="string raw1"
-adminPageId="adminpage/storefront/action"
-
-test_admin_html_page_match "$moduleName" "$moduleGroup" "$testDesc" "$adminPageId" "$matchRule"
